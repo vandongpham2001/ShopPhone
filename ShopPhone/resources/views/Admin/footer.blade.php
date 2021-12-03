00d67be2320d1410c50ec660fcfc16aab93964f0
vandongphamtk2001 <vandongphamtk282001@gmail.com>
2021-12-04 01:46:57 +0700

    first commit

---
+++ ShopPhone/resources/views/Admin/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="/backend/admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/backend/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/backend/admin/dist/js/adminlte.min.js"></script>
